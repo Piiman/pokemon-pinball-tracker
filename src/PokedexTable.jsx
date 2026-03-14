02c96057d256925f98c9a67dd494bb44010c2b48
Miguel Palomar <palomar.miguel2d@gmail.com>
2026-03-14 07:43:57 +0000

    add table sort

--- src/PokedexTable.jsx
+++ src/PokedexTable.jsx
@@ -1,46 +1,57 @@
 import { useState } from 'react';
 import cx from 'clsx';
-import { ScrollArea, Table, Checkbox, Container, Image, Flex } from '@mantine/core';
-import { useLocalStorage } from '@mantine/hooks';
-import { IconPokeball} from '@tabler/icons-react';
+import { ScrollArea, Table, Checkbox, Container, Image, Flex, Group } from '@mantine/core';
+import { readLocalStorageValue, useLocalStorage } from '@mantine/hooks';
+import { IconCaretDownFilled, IconCaretUpFilled, IconCaretUpDown, IconPokeball} from '@tabler/icons-react';
 import classes from './TableScrollArea.module.css';
 
 import pokedex from './pokedex.json';
 
 export function PokedexTable() {
   const [scrolled, setScrolled] = useState(false);
+  const [sortedData, setSortedData] = useState(pokedex);
+  const [sortReversed, setSortReversed] = useState(false);
+  const [sortBy, setSortBy] = useState("id");
 
   const [captured, setCaptured] = useLocalStorage({
     key: 'captured-id-list',
     defaultValue: [],
   });
 
-  const [missing] = useLocalStorage({
-    key: 'missing-filter',
-  });
+  const missing = readLocalStorageValue({key: 'missing-filter'});
 
-  const [eReader] = useLocalStorage({
-    key: 'e-reader-filter',
-  });
+  const eReader = readLocalStorageValue({key: 'e-reader-filter'});
 
-  const [poke] = useLocalStorage({
-    key: 'name-filter',
-  });
+  const poke = readLocalStorageValue({key: 'name-filter'});
 
-  const [field] = useLocalStorage({
-    key: 'field-filter',
-  });
+  const field = readLocalStorageValue({key: 'field-filter'});
 
-  const [area] = useLocalStorage({
-    key: 'area-filter',
-  });
+  const area = readLocalStorageValue({key: 'area-filter'});
 
   const updateCaptures = (id) =>
     setCaptured((current) =>
       current.includes(id) ? current.filter((item) => item !== id) : [...current, id]
     );
 
-  const rows = pokedex.map((row) => {
+  const sortPokedex = (column, sortReversed) => {
+    return [...pokedex].sort((a, b) => {
+        if (sortReversed) {
+            return column == "area" ? b[column][0].localeCompare(a[column][0]) : b[column].localeCompare(a[column]);
+          }
+    
+          return column == "area" ? a[column][0].localeCompare(b[column][0]) :  a[column].localeCompare(b[column]);
+      })
+  }
+
+  const setSortTable = (column) => {
+    const reversed = column === sortBy ? !sortReversed : false;
+    setSortReversed(reversed);
+    setSortBy(column);
+
+    setSortedData(sortPokedex(column, reversed));
+  }
+
+  const rows = sortedData.map((row) => {
     if (!eReader && row.id > 201) return null;
 
     if (field != undefined && field.length != 0 && !field.includes(row.board)) return null;
@@ -92,12 +103,12 @@ export function PokedexTable() {
         <Table.Thead bg="red" className={cx(classes.header, { [classes.scrolled]: scrolled })}>
           <Table.Tr>
             <Table.Th w={0}></Table.Th>
-            <Table.Th visibleFrom="sm">Id</Table.Th>
-            <Table.Th>Pokemon</Table.Th>
-            <Table.Th>Field</Table.Th>
-            <Table.Th>Area</Table.Th>
-            <Table.Th>Arrows</Table.Th>
-            <Table.Th visibleFrom="sm">Prev. Evolution</Table.Th>
+            <Table.Th style={{cursor: "pointer"}} px={0} onClick={() => setSortTable("id")} visibleFrom="sm"><Group gap={0} align='center'>Id {sortBy != "id" ? <IconCaretUpDown/> : sortReversed ? <IconCaretUpFilled/> : <IconCaretDownFilled/>}</Group></Table.Th>
+            <Table.Th style={{cursor: "pointer"}} px={0} onClick={() => setSortTable("name")}><Group gap={0} align='center'>Pokemon {sortBy != "name" ? <IconCaretUpDown/> : sortReversed ? <IconCaretUpFilled/> : <IconCaretDownFilled/>}</Group></Table.Th>
+            <Table.Th style={{cursor: "pointer"}} px={0} onClick={() => setSortTable("board")}><Group gap={0} align='center'>Field {sortBy != "board" ? <IconCaretUpDown/> : sortReversed ? <IconCaretUpFilled/> : <IconCaretDownFilled/>}</Group></Table.Th>
+            <Table.Th style={{cursor: "pointer"}} px={0} onClick={() => setSortTable("area")}><Group gap={0} align='center'>Area {sortBy != "area" ? <IconCaretUpDown/> : sortReversed ? <IconCaretUpFilled/> : <IconCaretDownFilled/>}</Group></Table.Th>
+            <Table.Th style={{cursor: "pointer"}} px={0} onClick={() => setSortTable("arrows")}><Group gap={0} align='center'>Arrows {sortBy != "arrows" ? <IconCaretUpDown/> : sortReversed ? <IconCaretUpFilled/> : <IconCaretDownFilled/>}</Group></Table.Th>
+            <Table.Th style={{cursor: "pointer"}} px={0} onClick={() => setSortTable("pre")} visibleFrom="sm"><Group gap={0} align='center'>Prev. Evolution {sortBy != "pre" ? <IconCaretUpDown/> : sortReversed ? <IconCaretUpFilled/> : <IconCaretDownFilled/>}</Group></Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{rows}</Table.Tbody>
